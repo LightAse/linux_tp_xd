fa3d720515db108b7eb9c300fc1da9dea87c951e
LightAse <111392061+LightAse@users.noreply.github.com>
2023-11-09 16:45:00 -0300

    Update esLaborable2.sh

--- esLaborable2.sh
+++ esLaborable2.sh
@@ -5,14 +5,6 @@ esTipoDia() {
     fecha="$1"
     lista_feriados=("$2")
 
-    dia_semana=$(date -d "$fecha" +%u)
-
-    # Verifica si la fecha es un día no laborable (fin de semana)
-    if [ $dia_semana -ge 6 ]; then
-        echo "La fecha $fecha es un día no laborable (fin de semana)."
-        return
-    fi
-
     # Resto de tu función esTipoDia (modificado para Bash)
     fecha_obj=$(date -d "$fecha" +"%Y-%m-%d")
 
@@ -26,8 +18,8 @@ esTipoDia() {
 # Descarga la lista de feriados desde la URL proporcionada
 url_feriados='http://servicios.infoleg.gob.ar/infolegInternet/anexos/170000-174999/174389/norma.htm'
 
-# Extrae las fechas del formato con nombres de meses
-lista_feriados=($(curl -s "$url_feriados" | grep -oP '\d{1,2} de [^\s]+'))
+# Extrae las fechas con nombres de meses
+lista_feriados=($(curl -s "$url_feriados" | grep -oP '(\d{1,2} de [^\s]+)|(^\d{1,2} de [^\s]+)'))
 
 # Obtén la fecha como argumento de la consola
 fecha_consola="$1"
